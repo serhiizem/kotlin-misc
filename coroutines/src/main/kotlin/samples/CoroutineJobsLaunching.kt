0301d90c5923c6b6f22f46cc8a87344a64291ec1
Serhii Zemlianyi <serhiizem@gmail.com>
2024-01-14 18:29:19 +0200

    basic example of launching coroutine jobs

---
+++ coroutines/src/main/kotlin/samples/CoroutineJobsLaunching.kt
@@ -0,0 +1,25 @@
+package samples
+
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+
+const val num_tasks = 10_000
+const val num_loops = 500
+const val delay_ms = 10L
+
+fun main() = runBlocking {
+    val counter = AtomicInteger()
+    val jobs = mutableListOf<Job>()
+
+    for (i in 1..num_tasks) {
+        jobs.add(launch(Dispatchers.IO) {
+            for (x in 1..num_loops) {
+                delay(delay_ms)
+            }
+            counter.getAndIncrement()
+        })
+    }
+
+    jobs.forEach { it.join() }
+    println(counter.get())
+}
